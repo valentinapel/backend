78cb25c2832548ae98bef65c85a28108fdeee8e0
bakaren <46865323+bakaren@users.noreply.github.com>
2023-11-21 16:34:17 +0100

    Fix missing import

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 import express from'express';
-import {login, register} from '../controllers/auth.controller.js';
+import {login, register, registerAdmin} from '../controllers/auth.controller.js';
 const router =express.Router();
 
 //registration routing

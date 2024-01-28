c351270212e37c45d4c35ab04f20c0b085fa7b83
bakaren <46865323+bakaren@users.noreply.github.com>
2024-01-28 14:21:00 +0100

    Fix user creation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,6 +20,7 @@ export const register = async (req,res,next)=> {
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
+            isAdmin: req.body.isAdmin,
             password: hashPassword,
             roles: []
         });
